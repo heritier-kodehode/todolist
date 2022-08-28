b17c255758ce61aa43ee32d1c939cca2fe510282
heritier-kodehode <106584001+heritier-kodehode@users.noreply.github.com>
2022-08-28 20:55:30 +0200

    add files via upload
    added comments to js file

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
+/**Selection of dom elements */
 const formEl = document.getElementById("form");
 const titleInputEl = document.getElementById("titleinput");
 const infoInputEl = document.getElementById("infoinput");
 const errorMsgEl = document.querySelector("#errormsgbox");
 const errorMsgBtn = document.querySelector("#errormsgboxbtn");
 const itemsList = document.querySelector(".itemsList");
-
+/**Initializing Todolist array and todays date */
 let todoItems = getFromLocalStorage() || [];
 let today = formatDate(new Date);
-
+/**initializing items in localstorage if any */
 renderItems();
 
-
+//Eventlistener for adding submited items into the todoitems array
 formEl.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (titleInputEl.value.length < 3 || infoInputEl.value.length < 3) {
+  e.preventDefault(); //stop site from refresh
+
+  if (titleInputEl.value.length < 3 || infoInputEl.value.length < 3) {//display error msg and return if text length to short
     errorMsgEl.style.display = "block"
     return
   };
 
-  todoItems.push({
+  todoItems.push({ //Push the items into the todoitems array
     id: Date.now(),
     title: titleInputEl.value,
     info: infoInputEl.value,
     date: today
   })
-  console.log(titleInputEl.value)
-  console.log(infoInputEl.value)
+
   titleInputEl.value = "";
   infoInputEl.value = "";
   sendToLocalStorage(todoItems);
   renderItems();
 })
 
+//Error message listener if text to short
 errorMsgBtn.addEventListener("click", () => {
   errorMsgEl.style.display = "none";
 })
 
+//Creating dom items and rendering them with appropriate textcontent and appeding them
 function renderItems() {
   itemsList.textContent = "";
   for (let i = 0; i < todoItems.length; i++) {
+    //for every item in the array we create the dom elements and push in appropriate textcontent for the current item
     let itemContainer = document.createElement("div");
     itemContainer.classList.add("item", "popheight");
     
@@ -57,6 +61,7 @@ function renderItems() {
     itemBtn.className = "removebtn";
     itemBtn.textContent = "X";
     itemBtn.id = todoItems[i].id;
+    //append the children into the appropriate containers
     itemContentContainer.append(itemTitle);
     itemContentContainer.append(itemInfo);
     itemContentContainer.append(itemDateFormat);
@@ -64,12 +69,13 @@ function renderItems() {
     itemContainer.append(itemBtn);
     itemsList.append(itemContainer);
 
-    itemBtn.addEventListener("click", (e)=>{
+    itemBtn.addEventListener("click", (e)=>{//add eventlistener to removal button
       removeItem(todoItems[i].id);
     })
   }
 }
 
+//remove items based on id and update localstorage and rerender
 function removeItem(id){
     
     let newTodoItems = todoItems.filter((item) => {
@@ -78,17 +84,20 @@ function removeItem(id){
     todoItems = newTodoItems;
     sendToLocalStorage(todoItems);
     renderItems();
-    console.log(todoItems)
+
 }
 
+//Send to locastorage
 
 function sendToLocalStorage(ourItems){
    localStorage.setItem("todolist", JSON.stringify(todoItems));
 }
+//Get from localstorage
 function getFromLocalStorage(){
   return JSON.parse(localStorage.getItem("todolist"));
 }
 
+//format the date for item
 function formatDate(date) {
   return (
     date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
